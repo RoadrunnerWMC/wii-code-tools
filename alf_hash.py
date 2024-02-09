b0d60ffe0fbc91f5371baf63854c1322fdf388e0
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2024-02-09 02:25:08 -0500

    Add initial-seed parameter for alf_hash.py

--- alf_hash.py
+++ alf_hash.py
@@ -15,16 +15,21 @@ def main(args: Optional[List[str]] = None) -> None:
 
     parser.add_argument('string', nargs='?',
         help="the string to hash. It's a good idea to surround it in quotes (preferably single-quotes) so the shell doesn't eat special characters.")
+    parser.add_argument('start_hash', nargs='?',
+        help="the initial hash value to start with.")
 
     parsed_args = parser.parse_args(args)
+    seed = parsed_args.start_hash
+    if seed is None: seed = 0x1505
+    else: seed = int(seed, 16)
 
     if parsed_args.string is None:
         print('(Use Ctrl+C or Ctrl+D to exit)')
         while True:
-            hash = code_files.alf.hashes.hash(input('> '))
+            hash = code_files.alf.hashes.hash(input('> '), seed=seed)
             print(f'{hash:08x}')
     else:
-        hash = code_files.alf.hashes.hash(parsed_args.string)
+        hash = code_files.alf.hashes.hash(parsed_args.string, seed=seed)
         print(f'{hash:08x}')
 
 
